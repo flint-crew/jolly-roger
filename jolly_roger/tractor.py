fixed scaling error. now working wrap+taper

jolly_roger/tractor.py
@@ -75,10 +75,10 @@ def tukey_taper(
         x_freq = x_freq[:, None] - tukey_x_offset[None, :]
         from jolly_roger.wrap import symmetric_domain_wrap
 
-        x_freq = symmetric_domain_wrap(values=x_freq, upper_limit=np.max(x))
+        x_freq = symmetric_domain_wrap(values=x_freq, upper_limit=np.pi)
 
     taper = np.ones_like(x_freq)
-    logger.debug(f"{x_freq.shape=} {type(x_freq)=}")
+    # logger.debug(f"{x_freq.shape=} {type(x_freq)=}")
     # Fully zero region
     taper[np.abs(x_freq) > outer_width] = 0
 
@@ -540,22 +540,22 @@ def _tukey_tractor(
         # Calculate the offset account of nyquist sampling
         from jolly_roger.wrap import symmetric_domain_wrap
 
-        wrap_tukey_x_offset = symmetric_domain_wrap(
-            values=tukey_x_offset, upper_limit=np.max(delay_time.delay)
+        tukey_x_offset = symmetric_domain_wrap(
+            values=tukey_x_offset.value, upper_limit=np.max(delay_time.delay).value
         )
 
         # need to scale the x offsert to the -pi to pi
         # The delay should be symmetric
-        # tukey_x_offset = (
-        #     tukey_x_offset / (np.max(delay_time.delay) / np.pi).decompose()
-        # ).value
+        tukey_x_offset = (
+            tukey_x_offset / (np.max(delay_time.delay) / np.pi).decompose()
+        ).value
         # # logger.info(f"{tukey_x_offset=}")
 
     taper = tukey_taper(
         x=delay_time.delay,
         outer_width=tukey_tractor_options.outer_width,
         tukey_width=tukey_tractor_options.tukey_width,
-        tukey_x_offset=wrap_tukey_x_offset,
+        tukey_x_offset=tukey_x_offset,
     )
     if w_delays is not None:
         # The use of the `tukey_x_offset` changes the