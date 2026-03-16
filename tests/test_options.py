0a8deb723a624e1ff775f39b469c5d35bb76fbca
Alec Thomson <alec.thomson@skao.int>
2026-03-16 16:34:05 +0800

    Lint

--- tests/test_options.py
+++ tests/test_options.py
@@ -1,27 +1,23 @@
-import pytest
+from __future__ import annotations
 
+import pytest
 from pydantic import ValidationError
 
 from jolly_roger.tractor import TukeyTractorOptions
 
+
 def test_tractor_options():
 
     default_options = TukeyTractorOptions()
-    default_options_tweak = default_options.with_options(
-        output_column="DATA"
-    )
+    default_options_tweak = default_options.with_options(output_column="DATA")  # type: ignore[arg-type]
 
-    assert default_options.output_column == "CORRECTED_DATA"
+    assert default_options.output_column == "CORRECTED_sDATA"
     assert default_options_tweak.output_column == "DATA"
 
-    _ = TukeyTractorOptions(
-        target_objects=("sun", "SgrA", "CygA")
-    )
+    _ = TukeyTractorOptions(target_objects=("sun", "SgrA", "CygA"))
 
     with pytest.raises(ValidationError):
-        _ = TukeyTractorOptions(
-            target_objects=9000
-        )
-        _ = TukeyTractorOptions(
-            data_column=True
-        )
+        _ = TukeyTractorOptions(target_objects=9000)
+
+    with pytest.raises(ValidationError):
+        _ = TukeyTractorOptions(data_column=True)
