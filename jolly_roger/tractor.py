9bb160d73905df7956eacb065892d08b4b559fdf
tgalvin <tim.galvin@csiro.au>
2026-04-04 16:26:14 +0800

    toying around

--- jolly_roger/tractor.py
+++ jolly_roger/tractor.py
@@ -596,6 +596,21 @@ def _tukey_tractor(
     intersecting_taper = np.any(
         np.reshape((taper != 1) & (field_taper != 1), (taper.shape[0], -1)), axis=1
     )
+    
+    # Here we consider what to do if want to compare brightness of the object in delay
+    # space is less than that of the field. If the object is not detected we ought to 
+    # set the tape to 1 so the data are not modified
+    if tukey_tractor_options.compare_to_field:
+        abs_delay_time = np.abs(delay_time.delay_time)
+        field_sum = np.sum(
+            abs_delay_time * (1. - field_taper), axis=1
+        )
+        object_sum = np.sum(
+            abs_delay_time * (1. - taper), axis=1
+        )
+        taper[object_sum < tukey_tractor_options.compare_to_field * field_sum] = 1.0
+    
+    
     # # Should the data need to be modified in conjunction with the flags
     # taper[
     #     intersecting_taper &
@@ -700,6 +715,8 @@ class TukeyTractorOptions(BaseOptions):
     """Flip the sign of UVWs (required for LOFAR)"""
     max_workers: int = 1
     """The number of compute processes to establish. Each process gets chunk_size of rows. If max_worker==1 all work is performed in main thread."""
+    compare_to_field: float | None = None
+    """Compare the source brightness in delay space to the field. If the source is fainter than the field multipled by this factor, do not taper. Defaults to None."""
 
 
 @dataclass(frozen=True)
