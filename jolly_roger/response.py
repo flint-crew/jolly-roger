f6b8ef696e0f3ef99625218d9a9af55f700edee3
Tim Galvin <gal16b@petrichor-i3.cm.cluster>
2026-04-13 16:05:17 +1000

    added some herlper utilities

---
+++ jolly_roger/response.py
@@ -0,0 +1,69 @@
+"""Helper functions and utilities to assist with the management of sinc responses in delay space"""
+
+from __future__ import annotations
+
+import astropy.units as u
+import numpy as np
+from numpy.typing import NDArray
+
+from jolly_roger.logging import logger
+
+"""This section deals with attempting to anaylitically construct properties of an
+idealised sinc response from an impulse. The limits of normalised sinc response:
+
+>>> sin(pi * x) / (pi * x)
+
+spans +/- 1. If we can constructed the expected range in delay space we ought
+to be able to automatically set a taper width and predict the location of the
+N'th sidelobe.
+"""
+# TODO: The above needs some proper math refs to support. Got here through
+# through some reading and toying.
+
+
+def calculate_expected_sinc_width(
+    freqs: NDArray[np.floating] | u.Quantity,
+) -> u.Quantity:
+    """The expected width of a sinc response in delay space given a set of frequencies,
+
+    The return result is +/- from 0. In otherwords the domain of the normalised
+    sinc function is 2 * result. The width of the sinc response is determined by the
+    bandwidth of the frequencies.
+
+    Args:
+        freqs (NDArray[np.floating] | u.Quantity): The sampled frequencies. If units are not specified MHz are assumed.
+
+    Returns:
+        u.Quantity: The expected width of the sinc response
+    """
+    if not isinstance(freqs, u.Quantity):
+        freqs = freqs * u.MHz  # Assume MHz if no units are provided
+
+    bandwidth = np.max(freqs) - np.min(freqs)  # Calculate bandwidth in Hz
+    sinc_width = (
+        (1 / bandwidth).decompose().to("s")
+    )  # The width of the sinc response in seconds
+
+    logger.debug(f"Calculated expected sinc width: {sinc_width=} for {bandwidth=}")
+
+    return sinc_width
+
+
+def get_delay_of_nth_sidelobe(
+    n: int, sinc_width: u.Quantity | np.floating
+) -> u.Quantity:
+    """Calculate the expected delay of the N'th sidelobe of a sinc response given the width of the sinc response.
+
+    The N'th sidelobe is located at n * sinc_width. The first sidelobe is located at sinc_width, the second at 2 * sinc_width, and so on.
+
+    Args:
+        n (int): The order of the sidelobe (1 for first sidelobe, 2 for second, etc.)
+        sinc_width (u.Quantity | np.floating): The width of the sinc response in seconds. If no units are attached seconds are assumed.
+
+    Returns:
+        u.Quantity: The expected delay of the N'th sidelobe in seconds.
+    """
+    if not isinstance(sinc_width, u.Quantity):
+        sinc_width *= u.s
+
+    return sinc_width * (n + 0.5)
---
+++ tests/test_response.py
@@ -0,0 +1,109 @@
+"""Tests for the sinc response utilieis"""
+
+from __future__ import annotations
+
+import astropy.units as u
+import numpy as np
+
+from jolly_roger.response import (
+    calculate_expected_sinc_width,
+    get_delay_of_nth_sidelobe,
+)
+
+# Example code used to investigate and create tests
+"""
+import numpy as np
+import astropy.units as u
+import matplotlib.pyplot as plt
+
+no_chans = 288
+freqs = (np.arange(no_chans) + 800) * u.MHz
+bandpass = np.ones(no_chans)
+
+
+diff = (1 / (freqs.max() - freqs.min())).decompose().to("s").value
+
+fft_band = np.fft.fftshift(
+    np.fft.fft(bandpass, norm="backward")
+)
+fft_delay = np.fft.fftshift(
+    np.fft.fftfreq(no_chans, np.diff(freqs)[0])
+)
+
+offt_band = np.fft.fftshift(
+    np.fft.fft(bandpass, 10028, norm="backward"),
+)
+offt_delay = np.fft.fftshift(
+    np.fft.fftfreq(10028, np.diff(freqs)[0])
+)
+
+
+fig, ax = plt.subplots(1,1)
+
+
+ax.plot(
+    offt_delay.to("s").value,
+    np.abs(offt_band),
+    label="Top hat"
+)
+
+ax.plot(
+    fft_delay.to("s").value,
+    np.abs(fft_band),
+    label="Padded Tophat"
+)
+
+ax.axvline(-diff, color="black", label="Main lobe")
+ax.axvline(diff, color="black")
+
+ax.set(
+    xlim=[-0.5e-7, 0.5e-7],
+    xlabel="Delay / s",
+    ylabel="Power"
+)
+
+for i in range(1, 10):
+    space = diff * (i + 0.5)
+    ax.axvline(space, label="Sidelobe" if i==1 else None)
+    ax.axvline(-space)
+
+
+ax.legend()
+"""
+
+
+def test_calculate_sinc_width() -> None:
+    """Calculate the expected sinc response width in delay space from
+    a set of known frequencies"""
+
+    no_chans = 288
+    freqs = (np.arange(no_chans) + 800) * u.MHz
+
+    width = calculate_expected_sinc_width(freqs=freqs)
+
+    assert width == (3.4843205574912892e-09 * u.s)
+
+
+def test_calculate_sinc_width_no_unit() -> None:
+    """Calculate the expected sinc response width in delay space from
+    a set of known frequencies. Same input as above but do not attach
+    frequency information to input"""
+
+    no_chans = 288
+    freqs_mhz = np.arange(no_chans) + 800
+
+    width = calculate_expected_sinc_width(freqs=freqs_mhz)
+
+    assert width == (3.4843205574912892e-09 * u.s)
+
+
+def test_nth_sidelobe() -> None:
+    """Ensure that an appropriate sidelobe position can be obtained"""
+
+    width = 3.4843205574912892e-09 * u.s
+    nth_sidelobe_delay = get_delay_of_nth_sidelobe(sinc_width=width, n=1)
+
+    assert nth_sidelobe_delay == (5.226480836236934e-09 * u.s)
+
+    nth_sidelobe_delay = get_delay_of_nth_sidelobe(sinc_width=width, n=4)
+    assert nth_sidelobe_delay == (1.5679442508710802e-08 * u.s)
