52983e915382741092b1152f0566a85ea2e86431
Alec Thomson <alec.thomson@skao.int>
2026-07-09 21:34:04 +0800

    Finish tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,14 @@
-from importlib.resources import as_file, files
-from pathlib import Path
+from __future__ import annotations
+
 import pickle
 import shutil
+from importlib.resources import as_file, files
+from pathlib import Path
 from typing import Any
 
 import numpy as np
 import pytest
+from astropy.coordinates import EarthLocation
 
 
 @pytest.fixture
@@ -28,20 +31,35 @@ def ms_ant_xyz() -> np.ndarray:
     with as_file(resource) as path, np.load(path) as arr:
         return arr["arr_0"]
 
+
 @pytest.fixture
 def ms_b_idx() -> np.ndarray:
     resource = files("jolly_roger.data").joinpath("tests", "b_idx.npz")
     with as_file(resource) as path, np.load(path) as arr:
         return arr["arr_0"]
-    
+
+
 @pytest.fixture
 def ms_b_xyz() -> np.ndarray:
     resource = files("jolly_roger.data").joinpath("tests", "b_xyz.npz")
     with as_file(resource) as path, np.load(path) as arr:
         return arr["arr_0"]
-    
+
+
+@pytest.fixture
+def ms_w_delays() -> np.ndarray:
+    resource = files("jolly_roger.data").joinpath("tests", "w_delays.npz")
+    with as_file(resource) as path, np.load(path) as arr:
+        return arr["arr_0"]
+
+
 @pytest.fixture
 def ms_b_map() -> dict[Any, Any]:
     resource = files("jolly_roger.data").joinpath("tests", "b_map.pkl")
     with as_file(resource) as path, path.open("rb") as f:
         return pickle.load(f)
+
+
+@pytest.fixture
+def ms_location() -> EarthLocation:
+    return EarthLocation.from_geocentric(-2556146.7, 5097426.7, -2848333.2, unit="m")
